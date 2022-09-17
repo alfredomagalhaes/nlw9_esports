1835b7c770d07b11e4ca49df4e8a7dc3580a56a0
Alfredo Magalhães <alfredo.magalhaes@outlook.com>
2022-09-16 23:19:11 -0300

    Added modal to show Discord info

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute, useNavigation } from '@react-navigation/native'
@@ -10,10 +11,12 @@ import { GameParams } from '../../@types/navigation';
 import { THEME } from '../../theme';
 import { Heading } from '../../components/Heading';
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
-import { useEffect, useState } from 'react';
+import { DuoMatch } from '../../components/DuoMatch'
+
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [discordDuoSelected, setDiscordDuoSelected] = useState("");
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
@@ -21,6 +24,13 @@ export function Game() {
   function handleGoBack() {
     navigation.goBack();
   }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.0.20:3333/ads/${adsId}/discord`)
+    .then(response => response.json())
+    .then(data => setDiscordDuoSelected(data.discord))
+  }
+
 
   useEffect(()=>{
     fetch(`http://192.168.0.20:3333/games/${game.id}/ads`)
@@ -67,7 +77,7 @@ export function Game() {
           renderItem={({item}) => (
             <DuoCard 
               data={item}
-              onConnect={() => {}}
+              onConnect={() => getDiscordUser(item.id)}
 
             />
           )}
@@ -80,7 +90,11 @@ export function Game() {
             </Text>
           )}
         />
-
+        <DuoMatch
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
+        />
         
       </SafeAreaView>
     </Background>
